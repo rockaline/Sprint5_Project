644465e8a2f77a6a1c12b42a18a99b30c7509c70
rockaline <roquealine@hotmail.com>
2024-09-29 11:14:02 -0300

    Updates readme and implemetation at .py

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Sprint5_Project
 Sprint 5 Project - Triple10
+
+Descrição do projeto
+O foco desse projeto foi o de praticar tarefas comuns de engenharia de software, visando aprimorar e complementar habilidades de dados. 
+
+As tarefas realizadas foram: criar e gerenciar ambientes virtuais de Python, desenvolver um aplicativo web e implantá-lo em um serviço de nuvem, tornando-o acessível ao público.
+
+Acesso ao repositório no GitHub: https://github.com/rockaline/Sprint5_Project.git
+Acesso ao aplicativo: https://sprint5-project-webf.onrender.com/
--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import plotly.express as px
 st.header('Aplicativo do Projeto da Sprint 5')
 
 # lendo os dados
-st.write('Ainda não é um aplicativo funcional. Em construção.')
 car_data = pd.read_csv('vehicles.csv')
 hist_button = st.button('Criar histograma')  # criar um botão para histograma
 # criar um botão para dispersão
@@ -32,3 +31,20 @@ if disp_button:  # se o botão for clicado
 
     # exibir um gráfico Plotly interativo
     st.plotly_chart(fig_2, use_container_width=True)
+
+# Criar um selectbox para escolher o tipo de veículo
+vehicle_types = car_data['type'].unique()  # Obtém os tipos de veículos únicos
+selected_vehicle_type = st.selectbox(
+    'Selecione o tipo de veículo:', vehicle_types)
+
+# Filtrar os dados com base no tipo de veículo selecionado
+filtered_data = car_data[car_data['type'] == selected_vehicle_type]
+
+# Criar o gráfico de barras usando Plotly Express
+fig = px.bar(filtered_data, x='model',
+             title=f"Veículos do tipo {selected_vehicle_type} por modelo",
+             labels={'model': 'Modelo', 'count': 'Contagem'},
+             text_auto=True)
+
+# Exibir o gráfico interativo no Streamlit
+st.plotly_chart(fig, use_container_width=True)
